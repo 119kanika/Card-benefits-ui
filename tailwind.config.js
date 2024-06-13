ba0ab2523f9070da2d310b9284c0182e2765652f
119kanika <kanika.mahindroo@gmail.com>
2024-06-13 15:43:18 +0530

    border and image hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'box': '1px 2px 8px 4px rgba(99, 99, 99, 0.2)',
+        'shadowHover' : '0px 0px 8px 4px #ccf1ff',
+      }
+    },
   },
   plugins: [],
 }
